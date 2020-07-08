57148e5c90e13879faa4f355e3edcf6a04652e00
Shurygina-Jul <melam.2916@gmail.com>
2020-07-08 23:15:22 +0300

    callback from props

--- src/Redux/State.js
+++ src/Redux/State.js
@@ -23,4 +23,12 @@ let state = {
   },
   sidebar: {},
 };
+export let addPost = (postMessage) => {
+  let newPost = {
+    id: 3,
+    message: postMessage,
+    likes: 0,
+  };
+  state.profilePage.posts.push(newPost);
+};
 export default state;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import state from "./Redux/State";
+import { addPost } from "./Redux/State";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App state={state} />
+    <App state={state} addPost={addPost} />
   </React.StrictMode>,
   document.getElementById("root")
 );
