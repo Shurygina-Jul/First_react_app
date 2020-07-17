9af363afbe17759d14e8de0617fc3b6d3ddf7849
Shurygina-Jul <melam.2916@gmail.com>
2020-07-17 14:03:52 +0300

    action creator, action type

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
+
+import { addPostActionCreator } from "../../../Redux/State";
+import { updateNewPostActionCreator } from "../../../Redux/State";
+
 const MyPosts = (props) => {
   let postsElements = props.posts.map((p) => (
     <Post message={p.message} likes={p.likes} />
   ));
   let newPostElement = React.createRef();
+
   let addPost = () => {
-    props.dispatch({ type: "ADD-POST" });
+    props.dispatch(addPostActionCreator());
   };
   let onPostChange = () => {
     let text = newPostElement.current.value;
-    let action = { type: "UPDATE-NEW-POST-TEXT", newText: text };
+    let action = updateNewPostActionCreator(text);
     props.dispatch(action);
   };
   return (
--- src/Redux/State.js
+++ src/Redux/State.js
@@ -1,3 +1,6 @@
+const ADD_POST = "ADD-POS";
+const UPDATE_NEW_POST_TEXT = "E-NEW-POST-TEXT";
+
 let store = {
   _state: {
     profilePage: {
@@ -38,7 +41,7 @@ let store = {
   },
 
   dispatch(action) {
-    if (action.type === "ADD-POST") {
+    if (action.type === ADD_POST) {
       let newPost = {
         id: 3,
         message: this._state.profilePage.newPostText,
@@ -47,12 +50,17 @@ let store = {
       this._state.profilePage.posts.push(newPost);
       this._state.profilePage.newPostText = "";
       this._callSubscriber(this._state);
-    } else if (action.type === "UPDATE-NEW-POST-TEXT") {
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
       this._state.profilePage.newPostText = action.newText;
       this._callSubscriber(this._state);
     }
   },
 };
 
+export const addPostActionCreator = () => ({ type: ADD_POST });
+export const updateNewPostActionCreator = (text) => ({
+  type: UPDATE_NEW_POST_TEXT,
+  newText: text,
+});
 window.store = store;
 export default store;
