057016a11d2a7021ff95e058e7bf7e4442fc496e
Shurygina Julia <65460474+Shurygina-Jul@users.noreply.github.com>
2020-07-22 23:08:48 +0300

    копия состояния в ProfileReduser

--- src/index.js
+++ src/index.js
@@ -6,24 +6,15 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import store from "./Redux/redux-store";
-
 import { Provider } from "react-redux";
 
-let rerenderEntierTree = () => {
-  ReactDOM.render(
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>,
-    document.getElementById("root")
-  );
-};
-
-rerenderEntierTree();
-
-store.subscribe(() => {
-  rerenderEntierTree();
-});
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
