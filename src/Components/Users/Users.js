3f887292e82b9fec4d29f95078f3332f5ddda0ed
Shurygina Julia <65460474+Shurygina-Jul@users.noreply.github.com>
2020-07-31 15:36:53 +0300

    follow-unfollow api

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Users.module.css";
 import avatar from "../../asses/images/avatar.jpg";
 import { NavLink } from "react-router-dom";
+import * as axios from "axios";
 
 let Users = (props) => {
   let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -43,15 +44,46 @@ let Users = (props) => {
               {u.followed ? (
                 <button
                   onClick={() => {
-                    props.unfollow(u.id);
+                    axios
+                      .delete(
+                        `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+
+                        {
+                          withCredentials: true,
+                          headers: {
+                            "API-KEY": "5772b158-0a03-4b5b-b874-a53013c29b2d",
+                          },
+                        }
+                      )
+                      .then((response) => {
+                        if (response.data.resultCode == 0) {
+                          props.unfollow(u.id);
+                        }
+                      });
                   }}
                 >
+                  {" "}
                   Unfollow
                 </button>
               ) : (
                 <button
                   onClick={() => {
-                    props.follow(u.id);
+                    axios
+                      .post(
+                        `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+                        {},
+                        {
+                          withCredentials: true,
+                          headers: {
+                            "API-KEY": "5772b158-0a03-4b5b-b874-a53013c29b2d",
+                          },
+                        }
+                      )
+                      .then((response) => {
+                        if (response.data.resultCode == 0) {
+                          props.follow(u.id);
+                        }
+                      });
                   }}
                 >
                   Follow
