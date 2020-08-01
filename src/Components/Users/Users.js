01cc98554177b4a85d213a504e1541dcbb276ce8
Shurygina Julia <65460474+Shurygina-Jul@users.noreply.github.com>
2020-08-01 14:42:25 +0300

    button disabled (follow\unfollow)

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -3,10 +3,120 @@ import styles from "./Users.module.css";
 import avatar from "../../asses/images/avatar.jpg";
 import { NavLink } from "react-router-dom";
 import * as axios from "axios";
+import { toggleFollowigProgress } from "../../Redux/users-reduser";
+import { followingInProgress } from "../../Redux/users-reduser";
+
+// let Users = (props) => {
+//   let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+//   let pages = [];
+//   for (let i = 1; i <= pagesCount; i++) {
+//     pages.push(i);
+//   }
+//   return (
+//     <div>
+//       <div>
+//         {pages.map((p) => {
+//           return (
+//             <span
+//               className={props.currentPage === p && styles.selectedPage}
+//               onClick={(e) => {
+//                 props.onPageChanged(p);
+//               }}
+//             >
+//               {p}
+//             </span>
+//           );
+//         })}
+//       </div>
+//       {props.users.map((u) => (
+//         <div key={u.id}>
+//           <span>
+//             <div>
+//               <NavLink to={"/profile/" + u.id}>
+//                 <img
+//                   src={u.photos.small != null ? u.photos.small : avatar}
+//                   className={styles.userPhoto}
+//                 />
+//               </NavLink>
+//             </div>
+//             <div>
+//               {u.followed ? (
+//                 // ? <button onClick={() => {
+
+//                 <button
+//                   disabled={props.followingInProgress.some((id) => id === u.id)}
+//                   onClick={() => {
+//                     props.toggleFollowingProgress(true, u.id);
+//                     axios
+//                       .delete(
+//                         `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+//                         {
+//                           withCredentials: true,
+//                           headers: {
+//                             "API-KEY": "5772b158-0a03-4b5b-b874-a53013c29b2d",
+//                           },
+//                         }
+//                       )
+//                       .then((response) => {
+//                         if (response.data.resultCode == 0) {
+//                           props.unfollow(u.id);
+//                         }
+//                         props.toggleFollowingProgress(false, u.id);
+//                       });
+//                   }}
+//                 >
+//                   Unfollow
+//                 </button>
+//               ) : (
+//                 // : <button onClick={() => {
+
+//                 <button
+//                   disabled={props.followingInProgress.some((id) => id === u.id)}
+//                   onClick={() => {
+//                     props.toggleFollowingProgress(true, u.id);
+//                     axios
+//                       .post(
+//                         `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+//                         {},
+//                         {
+//                           withCredentials: true,
+//                           headers: {
+//                             "API-KEY": "5772b158-0a03-4b5b-b874-a53013c29b2d",
+//                           },
+//                         }
+//                       )
+//                       .then((response) => {
+//                         if (response.data.resultCode == 0) {
+//                           props.follow(u.id);
+//                         }
+//                         props.toggleFollowingProgress(false, u.id);
+//                       });
+//                   }}
+//                 >
+//                   Follow
+//                 </button>
+//               )}
+//             </div>
+//           </span>
+//           <span>
+//             <span>
+//               <div>{u.name}</div>
+//               <div>{u.status}</div>
+//             </span>
+//             <span>
+//               <div>{"u.location.country"}</div>
+//               <div>{"u.location.city"}</div>
+//             </span>
+//           </span>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+// export default Users;
 
 let Users = (props) => {
   let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
-
   let pages = [];
   for (let i = 1; i <= pagesCount; i++) {
     pages.push(i);
@@ -28,7 +138,6 @@ let Users = (props) => {
           );
         })}
       </div>
-
       {props.users.map((u) => (
         <div key={u.id}>
           <span>
@@ -43,11 +152,12 @@ let Users = (props) => {
             <div>
               {u.followed ? (
                 <button
+                  disabled={props.followingInProgress.some((id) => id === u.id)}
                   onClick={() => {
+                    props.toggleFollowingProgress(true, u.id);
                     axios
                       .delete(
                         `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
-
                         {
                           withCredentials: true,
                           headers: {
@@ -59,15 +169,17 @@ let Users = (props) => {
                         if (response.data.resultCode == 0) {
                           props.unfollow(u.id);
                         }
+                        props.toggleFollowigProgress(false, u.id);
                       });
                   }}
                 >
-                  {" "}
                   Unfollow
                 </button>
               ) : (
                 <button
+                  disabled={props.followingInProgress.some((id) => id === u.id)}
                   onClick={() => {
+                    props.toggleFollowingProgress(true, u.id);
                     axios
                       .post(
                         `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
@@ -83,6 +195,7 @@ let Users = (props) => {
                         if (response.data.resultCode == 0) {
                           props.follow(u.id);
                         }
+                        props.toggleFollowingProgress(false, u.id);
                       });
                   }}
                 >
--- src/Components/Users/UsersContainer.jsx
+++ src/Components/Users/UsersContainer.jsx
@@ -8,6 +8,7 @@ import {
   setTotalUsersCount,
   toggleIsFetching,
 } from "./../../Redux/users-reduser";
+import { toggleFollowingProgress } from "./../../Redux/users-reduser";
 import * as axios from "axios";
 import Users from "./Users";
 import Preloader from "../Common/Preloader/Preloader";
@@ -44,6 +45,8 @@ class UsersContainer extends React.Component {
           users={this.props.users}
           unfollow={this.props.unfollow}
           follow={this.props.follow}
+          toggleFollowingProgress={this.props.toggleFollowingProgress}
+          followingInProgress={this.props.followingInProgress}
         />
       </>
     );
@@ -57,31 +60,9 @@ let mapStateToProps = (state) => {
     totalUsersCount: state.usersPage.totalUsersCount,
     currentPage: state.usersPage.currentPage,
     isFetching: state.usersPage.isFetching,
+    followingInProgress: state.usersPage.followingInProgress,
   };
 };
-
-/*let mapDispatchToProps = (dispatch) => {
-  return {
-    follow: (userId) => {
-      dispatch(followAC(userId));
-    },
-    unfollow: (userId) => {
-      dispatch(unfollowAC(userId));
-    },
-    setUsers: (users) => {
-      dispatch(setUsersAC(users));
-    },
-    setCurrentPage: (pageNumber) => {
-      dispatch(setCurrentPageAC(pageNumber));
-    },
-    setTotalUsersCount: (totalCount) => {
-      dispatch(setUsersTotalCountAC(totalCount));
-    },
-    toggleIsFetching: (isFetching) => {
-      dispatch(toggleIsFetchingAC(isFetching));
-    },
-  };
-};*/
 
 export default connect(mapStateToProps, {
   follow,
@@ -90,4 +71,5 @@ export default connect(mapStateToProps, {
   setCurrentPage,
   setTotalUsersCount,
   toggleIsFetching,
+  toggleFollowingProgress,
 })(UsersContainer);
