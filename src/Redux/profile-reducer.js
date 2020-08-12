2ff9d393d00a764d08d96883de722dd77f60d612
Shurygina Julia <65460474+Shurygina-Jul@users.noreply.github.com>
2020-08-12 20:55:05 +0300

    async  part 1

--- src/Redux/profile-reducer.js
+++ src/Redux/profile-reducer.js
@@ -75,18 +75,16 @@ export const getUserProfile = (userId) => (dispatch) => {
   });
 };
 
-export const getStatus = (userId) => (dispatch) => {
-  profileAPI.getStatus(userId).then((response) => {
-    dispatch(setStatus(response.data));
-  });
+export const getStatus = (userId) => async (dispatch) => {
+  let response = await profileAPI.getStatus(userId);
+  dispatch(setStatus(response.data));
 };
 
-export const updateStatus = (status) => (dispatch) => {
-  profileAPI.updateStatus(status).then((response) => {
-    if (response.data.resultCode === 0) {
-      dispatch(setStatus(status));
-    }
-  });
+export const updateStatus = (status) => async (dispatch) => {
+  let response = await profileAPI.updateStatus(status);
+  if (response.data.resultCode === 0) {
+    dispatch(setStatus(status));
+  }
 };
 
 export default profileReducer;
